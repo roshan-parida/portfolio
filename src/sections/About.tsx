bbfc741767004de154e467ebc42e893223fa2cc8
Roshan Parida <roshanparida24@gmail.com>
2024-09-19 17:27:20 +0530

    Upadated: footer links, Changed: small tweaks in about

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import dotImage from "@/assets/images/dotted.png";
 import roshanImg from "@/assets/images/roshan.jpg";
 import bookImage from "@/assets/images/lookback.png";
 import { SectionHeader } from "@/components/SectionHeader";
@@ -11,188 +12,192 @@ import { motion } from "framer-motion";
 import { useRef } from "react";
 
 const toolboxItems = [
-    {
-        title: "Javascipt",
-        iconType: "fluent:javascript-24-filled",
-    },
-    {
-        title: "HTML5",
-        iconType: "flowbite:html-solid",
-    },
-    {
-        title: "CSS3",
-        iconType: "flowbite:css-solid",
-    },
-    {
-        title: "Github",
-        iconType: "flowbite:github-solid",
-    },
-    {
-        title: "React",
-        iconType: "flowbite:react-solid",
-    },
-    {
-        title: "C/C++",
-        iconType: "cib:cplusplus",
-    },
-    {
-        title: "Python",
-        iconType: "cib:python",
-    },
+	{
+		title: "Javascipt",
+		iconType: "fluent:javascript-24-filled",
+	},
+	{
+		title: "HTML5",
+		iconType: "flowbite:html-solid",
+	},
+	{
+		title: "CSS3",
+		iconType: "flowbite:css-solid",
+	},
+	{
+		title: "Github",
+		iconType: "flowbite:github-solid",
+	},
+	{
+		title: "React",
+		iconType: "flowbite:react-solid",
+	},
+	{
+		title: "C/C++",
+		iconType: "cib:cplusplus",
+	},
+	{
+		title: "Python",
+		iconType: "cib:python",
+	},
 ];
 
 const hobbies = [
-    {
-        title: "Reading",
-        icon: "fluent:reading-list-24-filled",
-        left: "5%",
-        top: "5%",
-    },
-    {
-        title: "Sketching",
-        icon: "ri:sketching",
-        left: "65%",
-        top: "5%",
-    },
-    {
-        title: "Web-Series",
-        icon: "jam:screen",
-        left: "10%",
-        top: "35%",
-    },
-    {
-        title: "Movies",
-        icon: "jam:screen",
-        left: "35%",
-        top: "5%",
-    },
-    {
-        title: "Anime",
-        icon: "jam:screen",
-        left: "45%",
-        top: "35%",
-    },
-    {
-        title: "Journaling",
-        icon: "fluent:pen-24-filled",
-        left: "20%",
-        top: "65%",
-    },
-    {
-        title: "Games",
-        icon: "fluent:games-24-filled",
-        left: "75%",
-        top: "35%",
-    },
+	{
+		title: "Reading",
+		icon: "fluent:reading-list-24-filled",
+		left: "5%",
+		top: "10%",
+	},
+	{
+		title: "Sketching",
+		icon: "ri:sketching",
+		left: "65%",
+		top: "10%",
+	},
+	{
+		title: "Web-Series",
+		icon: "jam:screen",
+		left: "10%",
+		top: "40%",
+	},
+	{
+		title: "Movies",
+		icon: "jam:screen",
+		left: "35%",
+		top: "10%",
+	},
+	{
+		title: "Anime",
+		icon: "jam:screen",
+		left: "45%",
+		top: "40%",
+	},
+	{
+		title: "Journaling",
+		icon: "fluent:pen-24-filled",
+		left: "20%",
+		top: "65%",
+	},
+	{
+		title: "Games",
+		icon: "fluent:games-24-filled",
+		left: "75%",
+		top: "35%",
+	},
 ];
 
 export const About = () => {
-    const constraintRef = useRef(null);
+	const constraintRef = useRef(null);
 
-    return (
-        <section id="about" className="py-20 lg:py-28">
-            <div className="container">
-                <SectionHeader
-                    eyebrow="About Me"
-                    title="Glimpse Into My World"
-                    description="Learn mroe about who I am, what I do and what inspire me"
-                />
+	return (
+		<section id="about" className="py-20 lg:py-28">
+			<div className="container">
+				<SectionHeader
+					eyebrow="About Me"
+					title="Glimpse Into My World"
+					description="Learn mroe about who I am, what I do and what inspire me"
+				/>
 
-                <div className="mt-20 flex flex-col gap-8">
-                    <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
-                        <Card className="h-[320px] col-span-2 lg:col-span-1">
-                            <CardHeader
-                                title="My Reads"
-                                description="Explore the books shaping my perspectives."
-                            />
+				<div className="mt-20 flex flex-col gap-8">
+					<div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+						<Card className="h-[320px] md:col-span-2 lg:col-span-1">
+							<CardHeader
+								title="My Reads"
+								description="Explore the books shaping my perspectives."
+							/>
 
-                            <div className="w-40 mx-auto mt-2 md:mt-0">
-                                <Image src={bookImage} alt="Book Cover" />
-                            </div>
-                        </Card>
+							<div className="w-40 mx-auto mt-2 md:mt-0">
+								<Image src={bookImage} alt="Book Cover" />
+							</div>
+						</Card>
 
-                        <Card className="h-[320px] col-span-3 lg:col-span-2">
-                            <CardHeader
-                                title="My Toolbox"
-                                description="Explore the technologies and tools used to craft expectional digital experiences."
-                                className=""
-                            />
+						<Card className="h-[320px] md:col-span-3 lg:col-span-2">
+							<CardHeader
+								title="My Toolbox"
+								description="Explore the technologies and tools used to craft expectional digital experiences."
+								className=""
+							/>
 
-                            <ToolboxItem
-                                toolboxItems={toolboxItems}
-                                className=""
-                                itemsWrapperClassName="animate-move-left [animation-duration:30s]"
-                            />
+							<ToolboxItem
+								toolboxItems={toolboxItems}
+								className=""
+								itemsWrapperClassName="animate-move-left [animation-duration:30s]"
+							/>
 
-                            <ToolboxItem
-                                toolboxItems={toolboxItems}
-                                className="mt-6"
-                                itemsWrapperClassName="animate-move-right [animation-duration:15s]"
-                            />
-                        </Card>
-                    </div>
+							<ToolboxItem
+								toolboxItems={toolboxItems}
+								className="mt-6"
+								itemsWrapperClassName="animate-move-right [animation-duration:15s]"
+							/>
+						</Card>
+					</div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
-                        <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
-                            <CardHeader
-                                title="Beyond the Code"
-                                description="Explore my interests and hobbies beyond the code."
-                                className="px-6 py-6"
-                            />
+					<div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+						<Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+							<CardHeader
+								title="Beyond the Code"
+								description="Explore my interests and hobbies beyond the code."
+								className="px-6 py-6"
+							/>
 
-                            <div
-                                className="relative flex-1"
-                                ref={constraintRef}
-                            >
-                                {hobbies.map((hobby, idx) => (
-                                    <motion.div
-                                        key={idx}
-                                        className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-                                        style={{
-                                            left: hobby.left,
-                                            top: hobby.top,
-                                        }}
-                                        drag
-                                        dragConstraints={constraintRef}
-                                    >
-                                        <span className="font-medium text-gray-950">
-                                            {hobby.title}
-                                        </span>
-                                        <span>
-                                            <Icon
-                                                icon={hobby.icon}
-                                                className="size-6 text-gray-950"
-                                            />
-                                        </span>
-                                    </motion.div>
-                                ))}
-                            </div>
-                        </Card>
+							<div
+								className="relative flex-1 bg-opacity-5"
+								style={{
+									backgroundImage: `url(${dotImage.src})`,
+									backgroundSize: "15rem 15rem",
+								}}
+								ref={constraintRef}
+							>
+								{hobbies.map((hobby, idx) => (
+									<motion.div
+										key={idx}
+										className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+										style={{
+											left: hobby.left,
+											top: hobby.top,
+										}}
+										drag
+										dragConstraints={constraintRef}
+									>
+										<span className="font-medium text-gray-950">
+											{hobby.title}
+										</span>
+										<span>
+											<Icon
+												icon={hobby.icon}
+												className="size-6 text-gray-950"
+											/>
+										</span>
+									</motion.div>
+								))}
+							</div>
+						</Card>
 
-                        <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d448184.1134652341!2d77.12336456635295!3d28.64549338718438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1725697377184!5m2!1sen!2sin"
-                                allowFullScreen
-                                loading="lazy"
-                                referrerPolicy="no-referrer-when-downgrade"
-                                className="w-full h-full border-none object-cover rounded-3xl outline outline-2 -outline-offset-2 outline-gray-950/50 antialiased"
-                            ></iframe>
+						<Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+							<iframe
+								src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d448184.1134652341!2d77.12336456635295!3d28.64549338718438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1725697377184!5m2!1sen!2sin"
+								allowFullScreen
+								loading="lazy"
+								referrerPolicy="no-referrer-when-downgrade"
+								className="w-full h-full border-none object-cover rounded-3xl outline outline-2 -outline-offset-2 outline-gray-950/50 antialiased"
+							></iframe>
 
-                            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-24 rounded-full">
-                                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10 animate-ping [animation-duration:2s]"></div>
+							<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-24 rounded-full">
+								<div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10 animate-ping [animation-duration:2s]"></div>
 
-                                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+								<div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
 
-                                <Image
-                                    src={roshanImg}
-                                    alt="Image of roshan"
-                                    className="size-24 object-cover mb-2 rounded-full border-4 border-gray-700 shadow-lg antialiased"
-                                />
-                            </div>
-                        </Card>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+								<Image
+									src={roshanImg}
+									alt="Image of roshan"
+									className="size-24 object-cover mb-2 rounded-full border-4 border-gray-700 shadow-lg antialiased"
+								/>
+							</div>
+						</Card>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,53 +1,82 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 
-const footerLinks = [
-    {
-        title: "Twiter",
-        href: "youtube.com",
-    },
-    {
-        title: "Instagram",
-        href: "#",
-    },
-    {
-        title: "LinkedIn",
-        href: "#",
-    },
-    {
-        title: "GitHub",
-        href: "#",
-    },
+const scoialLinks = [
+	{
+		title: "roshan_ot23",
+		href: "https://instagram.com/roshan_ot23/",
+		icon: "bi:instagram",
+	},
+	{
+		title: "roshan_ot23",
+		href: "https://x.com/roshan_ot23",
+		icon: "bi:twitter-x",
+	},
+];
+
+const profLinks = [
+	{
+		title: "roshan-parida",
+		href: "https://linkedin.com/in/roshan-parida/",
+		icon: "bi:linkedin",
+	},
+	{
+		title: "roshan-parida",
+		href: "https://github.com/roshan-parida",
+		icon: "bi:github",
+	},
 ];
 
 export const Footer = () => {
-    return (
-        <footer className="relative -z-10 overflow-x-clip">
-            <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
-            <div className="container z-10">
-                <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8 z-10">
-                    <div className="text-white/40">
-                        &copy; 2024. All rights reserved.
-                    </div>
+	return (
+		<footer className="relative z-0 overflow-x-clip">
+			<div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+			<div className="container">
+				<div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+					<div className="text-white/40 font-mono cursor-default">
+						&copy; 2024. All rights reserved.
+					</div>
 
-                    <nav className="flex flex-col md:flex-row  items-center gap-8 z-10">
-                        {footerLinks.map((link, idx) => (
-                            <a
-                                key={idx}
-                                href={link.href}
-                                className="inline-flex items-center gap-1.5 z-10"
-                            >
-                                <span className="font-semibold z-10">
-                                    {link.title}
-                                </span>
-                                <Icon
-                                    icon="ph:arrow-circle-up-right-bold"
-                                    className="size-4"
-                                />
-                            </a>
-                        ))}
-                    </nav>
-                </div>
-            </div>
-        </footer>
-    );
+					<nav className="flex flex-col md:flex-row items-center gap-8">
+						<div className="flex items-center justify-center gap-3">
+							{scoialLinks.map((link, idx) => (
+								<a
+									key={idx}
+									href={link.href}
+									target="_blank"
+									className="inline-flex items-center gap-1"
+								>
+									<Icon
+										icon={link.icon}
+										className="size-5 hover:text-sky-400 hover:scale-110 transition-all ease-in-out"
+									/>
+								</a>
+							))}
+							<span className="font-semibold cursor-default bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text font-mono items-center justify-center">
+								roshan_ot23
+							</span>
+						</div>
+
+						<div className="flex items-center justify-center gap-3">
+							{profLinks.map((link, idx) => (
+								<a
+									key={idx}
+									href={link.href}
+									target="_blank"
+									className="inline-flex items-center gap-1"
+								>
+									<Icon
+										icon={link.icon}
+										className="size-5 hover:text-sky-400 hover:scale-110 transition-all ease-in-out"
+									/>
+								</a>
+							))}
+							<span className="font-semibold cursor-default bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text font-mono items-center justify-center">
+								roshan_parida
+							</span>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</footer>
+	);
 };
